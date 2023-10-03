128226a430ab21a3e19d0d02f45d56a10d89c62e
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-10-03 13:31:50 +0200

    Completed first Exercise

---
+++ Numero1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Numero1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci una parola e ti dirò se ha un numero pari o dispari di lettere:");
+        String word1 = input.nextLine();
+        System.out.println(stringaPariDispari(word1) ? word1 + " ha un numero pari di lettere." : word1 + " ha un numero dispari di lettere.");
+        System.out.println("Inserisci un anno e ti dirò se è bisestile o meno:");
+        int anno = Integer.parseInt(input.nextLine());
+        System.out.println(annoBisestile(anno) ? anno + " è bisestile." : anno + " non è bisestile.");
+
+        input.close();
+    }
+
+    public static boolean stringaPariDispari(String str) {
+        return str.length() % 2 == 0;
+    }
+
+    public static boolean annoBisestile(int n) {
+        boolean condition;
+        if (n % 4 == 0) {
+            if (n % 100 == 0 && n % 400 == 0) {
+                condition = true;
+            } else if (n % 100 != 0) {
+                condition = true;
+            } else {
+                condition = false;
+            }
+        } else {
+            condition = false;
+
+        }
+        return condition;
+    }
+}
